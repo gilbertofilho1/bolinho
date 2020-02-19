d64407c700925ba47fa3f98b26453eef17e6ce68
Gilberto <gilberttogil.gg@gmail.com>
2020-02-19 09:28:13 -0300

    Build

---
+++ web/build/precache-manifest.3b01df8fad904b7f65782e5881886b68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8e7a5e4ddd65184423b08dd6584b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490e19fce4a4c6618396",
+    "url": "/static/css/main.cf6fe65b.chunk.css"
+  },
+  {
+    "revision": "b911fddccfec31746d95",
+    "url": "/static/js/2.2f1d7015.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2f1d7015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490e19fce4a4c6618396",
+    "url": "/static/js/main.7da6e0b2.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  },
+  {
+    "revision": "8844aa8c775b2bb547361d190569d7e6",
+    "url": "/static/media/logo.8844aa8c.png"
+  }
+]);
